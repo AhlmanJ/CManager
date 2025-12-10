331908452ee8ecb9b0f8a0cae88fc44ee48d6518
AhlmanJ <johan.ahlman@hotmail.com>
2025-12-10 16:52:38 +0100

    Added an interface for the repository.

--- CManager.Application/Services/ICustomerSerice.cs
+++ CManager.Application/Services/ICustomerSerice.cs
@@ -8,11 +8,7 @@ public interface ICustomerSerice
 
     IEnumerable<CustomerModel> GetAllCustomers();
 
-    CustomerModel GetCustomerById(int Id);
-
     CustomerModel GetCustomerByName(string name);
-
-    CustomerModel UpdateCustomer(int Id, CustomerModel newCustomerModel);
 
     bool DeleteCustomer(string email);
 }
---
+++ CManager.Infrastructure/Repositories/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using CManager.Domain.Models;
+
+namespace CManager.Infrastructure.Repositories;
+
+public interface ICustomerRepository
+{
+    bool SaveCustomers(List<CustomerModel> Customers);
+    List<CustomerModel> GetAllCustomers();
+    CustomerModel GetCustomerByName(string name);
+    CustomerModel DeleteCustomer(string email);
+}
