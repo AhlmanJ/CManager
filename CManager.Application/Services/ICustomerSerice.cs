b840c7568b74578768c4a7f1c82e54ed229ede33
AhlmanJ <johan.ahlman@hotmail.com>
2025-12-11 02:22:07 +0100

    Created a "service" that handles the logic in my program.

--- CManager.Application/Services/ICustomerSerice.cs
+++ CManager.Application/Services/ICustomerSerice.cs
@@ -6,7 +6,7 @@ public interface ICustomerSerice
 {
     bool AddNewCustomer(string firstName, string lastName, string email, string phoneNr, string StreetAddress, string ZipCode, string City);
 
-    IEnumerable<CustomerModel> GetAllCustomers();
+    IEnumerable<CustomerModel> GetAllCustomers(out bool hasError);
 
     CustomerModel GetCustomerByName(string name);
 
--- CManager.Domain/Models/CustomerModel.cs
+++ CManager.Domain/Models/CustomerModel.cs
@@ -2,7 +2,7 @@
 
 public class CustomerModel
 {
-    public string Id { get; set; } = null!;
+    public Guid Id { get; set; }
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
