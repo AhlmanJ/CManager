51c9b6043e0de3c548fa7a49ed06ca778e43e6ac
AhlmanJ <johan.ahlman@hotmail.com>
2025-12-12 16:05:55 +0100

    Created the folder "Factories" in the application layer. In the folder I created a "Customer Factory".

---
+++ CManager.Application/Factories/CustomerFactory.cs
@@ -0,0 +1,29 @@
+﻿using CManager.Application.Helpers;
+using CManager.Domain.Models;
+using System.Reflection.Emit;
+
+namespace CManager.Application.Factories;
+
+public static class CustomerFactory
+{
+    public static CustomerModel Create(string firstName, string lastName, string email, string phoneNr, string streetAddress, string zipCode, string city)
+    {
+        CustomerModel customerModel = new()
+        {
+            Id = CustomerIdGenerator.GenerateGuidId(),
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email,
+            PhoneNr = phoneNr,
+            Address = new CustomerAddressModel
+            {
+                StreetAddress = streetAddress,
+                ZipCode = zipCode,
+                City = city
+            }
+        };
+
+        return customerModel;
+    }
+
+}
