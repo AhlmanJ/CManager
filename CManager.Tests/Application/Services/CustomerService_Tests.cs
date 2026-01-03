faaffc13f0101dd74d14fa250b9f742d7684d14f
AhlmanJ <johan.ahlman@hotmail.com>
2026-01-03 01:31:12 +0100

    Modified previous tests and added new tests for the service. Added some "Null" validation to the service. Changed "CreateCustomer" in both the service and repository. It is now the repository's responsibility to add the new customer to the "customer list". Made some changes to the "output text" for the Console App. Changed and added comments to the code.

--- CManager.Tests/Application/Services/CustomerService_Tests.cs
+++ CManager.Tests/Application/Services/CustomerService_Tests.cs
@@ -1,40 +1,29 @@
 ﻿
 /*
  
-To understand how tests work, I have used both the lectures at school, YouTube, searched for information on the internet and asked chatGPT for help.
+To understand how tests work, I have used both the lectures at school, YouTube, searched for information on the internet, and asked chatGPT for help and explanation.
+Unfortunately there was very limited information on the internet (that I could find),
+and therefore I have had to use chatGPT as a "teacher" who can break down code snippets and explain them to me. (In addition to the lectures at school).
+
+------ Test 1 ------ (Help by chatGPT)
+
+In my first test, I have received help from chatGPT on how to create a "mock" with NSubstitute and how to build the test to get a correct result.
 I asked chatGPT to "break down" the test, step by step in words and explain how to "mock" the repository.
-
-
------- Test 1 ------
-
-In this code I have followed the lecture "Simulera beroenden i samband med enhetstester (Unit testing + NSubstitute)" but I did not get it to work like in the video.
-I tried to create the parameters "FirstName", "LastName" etc.. by putting them in a variable "request" like in the video.
-To solve this I asked chatGPT how to do a test like this. 
-I had to create individual parameters instead of putting them in an object.
-I also got help with changing from "Add(Arg.Any<Member>())" to instead "calling" any list " Arg.Any<List<CustomerModel>>() " to "mock" the repository.
-
------- Test 2 ------
-
-No help with troubleshooting.
+In this test, I test both that the method in the service returns "true" and that the object sent to the repository has the same properties as the parameters sent into the service.  
 
 ------ Test 3 ------
 
-I got help troubleshooting chatGPT.
+In test 5, I had to find information on how to test a method that has a bool as the "output value". I couldn't find anything online and so had to let chatGPT explain this to me.
 
 ------ Test 4 ------
 
-In test 4, I had to find information on how to test a method that has a bool as the "output value". I couldn't find anything online and so had to let chatGPT explain this to me.
-
-It wasn't until now that I really understood how the whole test actually works. 
-How to think when creating a "Mock" repository etc... Unfortunately there was very limited information on the internet (that I could find),
-and therefore I have had to use chatGPT as a "teacher" who can break down code snippets and explain them to me. (In addition to the lectures at school).
-
------- Test 5 ------
-
 How to throw Exception - Learnt byt chatGPT. ( Throws(new Exception("DB error")); )
 
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------ Test 8 ------
 
+I got help by chatGPT to troubleshoot this test
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
  */
 
 using CManager.Business.Services;
@@ -43,35 +32,42 @@ using CManager.Infrastructure.Repositories;
 using NSubstitute;
 using NSubstitute.ExceptionExtensions;
 
+
 namespace CManager.Tests.Business.Services;
 
 public class CustomerService_Tests
 {
     // Test 1.
-
     [Fact]
 
-    public void CreateCustomer_ShouldReturn_True_IfCustomer_Is_Created()
+    public void CreateCustomer_ShouldReturn_True_IfCustomer_Was_Created()
     {
         // Arrange
         var customerRepository = Substitute.For<ICustomerRepository>();
-
-        // Calling on the method "GetAllCustomers()" that returns a list CustomerModel.
-        customerRepository.GetAllCustomers().Returns(new List<CustomerModel>());
-
-        // Calling on the method "CreateCustomer" with a list as parameter. 
-        customerRepository.CreateCustomer(Arg.Any<List<CustomerModel>>()).Returns(true);
-
         var customerService = new CustomerService(customerRepository);
 
         // Declaring the variables that i will use as parameters i "ACT".
         string firstName = "Firstname";
         string lastName = "Lastname";
         string email = "test@domain.com";
-        string phoneNr = "0123456789;";
+        string phoneNr = "0123456789";
         string streetAddress = "Street";
         string zipCode = "12345";
         string city = "City";
+
+        // Calling on the method "GetAllCustomers()" that returns a list CustomerModel.
+        customerRepository.GetAllCustomers().Returns(new List<CustomerModel>());
+
+        // Repository returns "true" if the CustomerModel object has the same properties as the parameters passed into the Service method CreateCustomer().
+        customerRepository.CreateCustomer(Arg.Is<CustomerModel>(c =>
+        c.FirstName == firstName &&
+        c.LastName == lastName &&
+        c.Email == email &&
+        c.PhoneNr == phoneNr &&
+        c.Address.StreetAddress == streetAddress &&
+        c.Address.ZipCode == zipCode &&
+        c.Address.City == city)).Returns(true);
+
 
         //Act
         var result = customerService.CreateCustomer(firstName, lastName, email, phoneNr, streetAddress, zipCode, city);
@@ -80,7 +76,90 @@ public class CustomerService_Tests
         Assert.True(result);
     }
 
-    // Test 2.
+    [Fact]
+
+    public void CreateCustomer_ShouldReturn_False_IfCustomer_WasNot_Created()
+    {
+        // Arrange
+        var customerRepository = Substitute.For<ICustomerRepository>();
+        var customerService = new CustomerService(customerRepository);
+
+        // Declaring the variables that i will use as parameters i "ACT".
+        string firstName = "Firstname";
+        string lastName = "Lastname";
+        string email = "test@domain.com";
+        string phoneNr = "0123456789";
+        string streetAddress = "Street";
+        string zipCode = "12345";
+        string city = "City";
+
+        // Calling on the method "GetAllCustomers()" that returns a list CustomerModel.
+        customerRepository.GetAllCustomers().Returns(new List<CustomerModel>());
+
+        // Repository will return "false".
+        customerRepository.CreateCustomer(Arg.Any<CustomerModel>()).Returns(false);
+
+        //Act
+        var result = customerService.CreateCustomer(firstName, lastName, email, phoneNr, streetAddress, zipCode, city);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+
+    // Test 3.
+    public void GetAllCustomers_ShouldReturn_ListOfCustomers_If_CustomersExists()
+    {
+
+        // Arrange
+        var customerRepository = Substitute.For<ICustomerRepository>();
+        var customerService = new CustomerService(customerRepository);
+
+        // Creating a list of customers that I use in my mock repository.
+        var customerList = new List<CustomerModel>
+        {
+            new CustomerModel { FirstName = "Firstname1", LastName = "Lastname1", Email = "Email1@Domain.com" },
+            new CustomerModel { FirstName = "Firstname2", LastName = "Lastname2", Email = "Email2@Domain.com" }
+        };
+
+        // "Mock"-repository "returns" a list of customers to my service.
+        customerRepository.GetAllCustomers().Returns(customerList);
+
+        // Act
+
+        // Run the method in service.
+        var customers = customerService.GetAllCustomers(out bool hasError);
+
+        // Assert
+        Assert.NotEmpty(customers);
+        Assert.False(hasError);
+        // Compare the customer list with the result "customers".
+        Assert.Equal(customerList, customers);
+    }
+
+    // Test 4.
+    [Fact]
+
+    public void GetAllCustomers_Bool_hasError_ShouldBecomeTrue_If_RepositoryFailsToLoadCustomerList()
+    {
+        // Arrange
+        var customerRepository = Substitute.For<ICustomerRepository>();
+
+        // How to throw Exception - Learnt byt chatGPT. ( Throws(new Exception("DB error")); )
+        customerRepository.GetAllCustomers().Throws(new Exception("DB error"));
+
+        var customerService = new CustomerService(customerRepository);
+
+        // Act
+        var result = customerService.GetAllCustomers(out bool hasError);
+
+        // Assert
+        Assert.True(hasError); // Check if hasError becomes True if repository throws an Exception.
+        Assert.Empty(result); // Check if the return value is empty when repository throws an Exception.
+        Assert.NotNull(result); // Make sure that the method does not return a Null value.
+        Assert.IsType<CustomerModel[]>(result); // Verify that the service returns an array.
+    }
 
     [Fact]
 
@@ -99,8 +178,42 @@ public class CustomerService_Tests
         Assert.True(result);
     }
 
-    // Test 3.
+    [Fact]
 
+    public void DeleteCustomer_ShouldReturn_False_IfTheCustomerList_IsEmpty_Or_IfNoCustomerWasFoundInTheList()
+    {
+        // Arrange
+        var customerRepository = Substitute.For<ICustomerRepository>();
+        var customerService = new CustomerService(customerRepository);
+        customerRepository.DeleteCustomer("name").Returns(false);
+
+        // Act
+        var result = customerService.DeleteCustomer("name");
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+
+    public void DeleteCustomer_ShouldReturn_False_IfEmailIsNull()
+    {
+        // Arrange
+
+        string email = null;
+
+        var customerRepository = Substitute.For<ICustomerRepository>();
+        var customerService = new CustomerService(customerRepository);
+        
+
+        // Act
+        var result = customerService.DeleteCustomer(email);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    // Test 8.
     [Fact]
 
     public void GetCustomerByEmail_ShouldReturn_CustomerModel_WhenCustomerFound()
@@ -108,99 +221,100 @@ public class CustomerService_Tests
 
         // ----------------------------------- I got help by chatGPT to troubleshoot this test --------------------------------------------------------
 
-        // Arrange
-
-        // Here I hadn't understood that I had to create a new instance of "new CustomerAddressModel". I had written "Address = { StreetAddress = " ", ... };
-        var customerModel = new CustomerModel() 
-        {   
-            FirstName = "FirstName", 
-            LastName = "LastName", 
-            Email = "test@domain.com", 
-            PhoneNr = "0123456789", 
-            Address = new CustomerAddressModel
-                { 
-                    StreetAddress = "StreetAddress",
-                    ZipCode = "12345",
-                    City = "City" 
-                }
-        };
-
-
-        var customerRepository = Substitute.For<ICustomerRepository>();
-
-        // Here I had written " Returns( new customerModel()); " , instead of customerModel.
-        customerRepository.GetCustomerByEmail("FirstName").Returns(customerModel);
-
-        var customerService = new CustomerService(customerRepository);
-
-        // Act
-        var result = customerService.GetCustomerByEmail("FirstName");
-
-        // Assert
-        Assert.NotNull(result);
-
-        //Checks that the text strings that I pass into the method are equal to those that are returned. (This i got information about from chatGPT)!
-        Assert.Equal("FirstName", result.FirstName);
-        Assert.Equal("LastName", result.LastName);
-
-
         // Error = System.NullReferenceException : Object reference not set to an instance of an object?
         // WHY? = Because I had not created a new instance of the "CustomerAddressModel" object.
-    }
-
-    [Fact]
-
-    // Test 4.
-
-    public void GetAllCustomers_ShouldReturn_ListOfCustomers_If_CustomersExists()
-    {
 
         // Arrange
-        var customerRepository = Substitute.For<ICustomerRepository>();
 
-        // Creating a list of customers that I use in my mock repository.
-        var customerList = new List<CustomerModel>
+        // Here I had not understood that I had to create a new instance of "new CustomerAddressModel". I had written "Address = { StreetAddress = " ", ... };
+        var customerModel = new CustomerModel()
         {
-            new CustomerModel { FirstName = "Firstname1", LastName = "Lastname1", Email = "Email1@Domain.com" },
-            new CustomerModel { FirstName = "Firstname2", LastName = "Lastname2", Email = "Email2@Domain.com" }
+            FirstName = "FirstName",
+            LastName = "LastName",
+            Email = "test@domain.com",
+            PhoneNr = "0123456789",
+            Address = new CustomerAddressModel
+            {
+                StreetAddress = "StreetAddress",
+                ZipCode = "12345",
+                City = "City"
+            }
         };
 
-        // "Mock"-repository "returns" a list of customers to my service.
-        customerRepository.GetAllCustomers().Returns(customerList);
-
+        var customerRepository = Substitute.For<ICustomerRepository>();
         var customerService = new CustomerService(customerRepository);
+
+        customerRepository.GetCustomerByEmail("test@domain.com").Returns(customerModel);
 
         // Act
 
-        // Run the method in service.
-        var customers = customerService.GetAllCustomers(out bool hasError);
+        var expectedCustomerModel = customerService.GetCustomerByEmail("test@domain.com");
 
         // Assert
-        Assert.NotEmpty(customers);
-        Assert.False(hasError);
-        // Compare the customer list with the result "customers".
-        Assert.Equal(customerList, customers);
+        Assert.Equal(expectedCustomerModel, customerModel);
     }
-
-    // Test 5.
 
     [Fact]
 
-    public void GetAllCustomers_Bool_hasError_ShouldBecomeTrue_If_RepositoryFailsToLoadCustomerList()
+    public void GetCustomerByEmail_ShouldThrowException_If_CustomerCouldNotBeFound()
+    {
+        // Arrange
+
+        var customerRepository = Substitute.For<ICustomerRepository>();
+        var customerService = new CustomerService(customerRepository);
+        string email = "test@domain.com";
+        customerRepository.GetCustomerByEmail(email).Throws(new Exception("DB error"));
+
+        // Act & Assert
+
+        Assert.Throws<Exception>(() => customerService.GetCustomerByEmail(email));
+    }
+
+    [Fact]
+
+    public void UpdateCustomer_ShouldReturn_False_If_CustomerIsNull()
     {
         // Arrange
         var customerRepository = Substitute.For<ICustomerRepository>();
-
-        // How to throw Exception - Learnt byt chatGPT. ( Throws(new Exception("DB error")); )
-        customerRepository.GetAllCustomers().Throws(new Exception("DB error"));
-
         var customerService = new CustomerService(customerRepository);
+        var customer = new CustomerModel();
+        
+        customerRepository.UpdateCustomer(customer).Returns(false);
 
         // Act
-        var result = customerService.GetAllCustomers(out bool hasError);
+        var result = customerService.UpdateCustomer(customer);
 
         // Assert
-        Assert.True(hasError);
+        Assert.False(result);
+    }
+
+    [Fact]
+
+    public void UpdateCustomer_ShouldReturn_True_If_TheCustomerIsUpdated()
+    {
+        // Arrange
+        var customerRepository = Substitute.For<ICustomerRepository>();
+        var customerService = new CustomerService(customerRepository);
+        var customer = new CustomerModel()
+        {
+            FirstName = "FirstName",
+            LastName = "LastName",
+            Email = "test@domain.com",
+            PhoneNr = "0123456789",
+            Address = new CustomerAddressModel
+            {
+                StreetAddress = "StreetAddress",
+                ZipCode = "12345",
+                City = "City"
+            }
+        };
+
+        customerRepository.UpdateCustomer(customer).Returns(true);
+
+        // Act
+        var result = customerService.UpdateCustomer(customer);
+
+        // Assert
+        Assert.True(result);
     }
 }
-
